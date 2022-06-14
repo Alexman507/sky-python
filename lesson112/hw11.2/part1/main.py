1f148fba86370ffd3d4a80a5ec80e57b332b033b
Alexman507 <Nerocross507@gmail.com>
2022-06-14 03:28:34 +0300

    create part1 fastest

---
+++ lesson112/hw11.2/part1/main.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def page_cat():
+    return render_template('card.html')
+
+
+if __name__ == "__main__":
+    app.run(host='127.0.0.1', port=8000, debug=True)
