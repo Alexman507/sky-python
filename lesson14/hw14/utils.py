edd60a3e73f5dc5df1b982b768ce6b9369d95aeb
Alexman507 <Nerocross507@gmail.com>
2022-07-04 16:28:22 +0300

    add ass

---
+++ lesson14/hw14/utils.py
@@ -0,0 +1,26 @@
+import sqlite3
+
+
+def get_value_from_db(sql_response):
+    with sqlite3.connect("../netflix.db") as connection:
+        connection.row_factory = sqlite3.Row
+
+        result = connection.execute(sql_response).fetchall()
+
+        return result
+
+
+def search_by_title(title):
+    sql = f"""
+        SELECT * FROM netflix
+        WHERE title = {title}
+        ORDER BY release_year DESC
+        LIMIT 1
+        """
+
+    result = get_value_from_db(sql)
+    for item in result:
+        return dict(item)
+
+
+
